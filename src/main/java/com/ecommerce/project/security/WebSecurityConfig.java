86ffaa1b020bb0cdb5281d8aabf2c335f1621bf7
V V SAI SRINIVAS TEJA <tsrinu8295@gmail.com>
2024-12-23 13:01:06 +0530

    init command line runner not needed

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.ecommerce.project.security;
 
+/*
 import com.ecommerce.project.model.AppRole;
 import com.ecommerce.project.model.Role;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.repositories.RoleRepository;
 import com.ecommerce.project.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+*/
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,7 +34,7 @@ import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
 import com.ecommerce.project.security.jwt.AuthTokenFilter;
 import com.ecommerce.project.security.services.UserDetailsServiceImpl;
 
-import java.util.Set;
+//import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -110,6 +112,7 @@ public class WebSecurityConfig {
     }
 
 
+    /*
     @Bean
     public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
@@ -170,5 +173,6 @@ public class WebSecurityConfig {
             });
         };
     }
+    */
 
 }
