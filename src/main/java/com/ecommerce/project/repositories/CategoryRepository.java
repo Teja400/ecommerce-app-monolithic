87afc538b7a4e839adb1d91afddefe734b78ba5d
V V SAI SRINIVAS TEJA <tsrinu8295@gmail.com>
2024-12-23 12:40:56 +0530

    Category repo updated according to cart

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     Category findByCategoryName(String categoryName);
+
 }
