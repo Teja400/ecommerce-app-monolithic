166f83f17575a5613ad670ec5896efbccc2088f0
V V SAI SRINIVAS TEJA <tsrinu8295@gmail.com>
2024-12-24 17:48:03 +0530

    Order MVC files created

---
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Order;
+import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.payload.OrderRequestDTO;
+import com.ecommerce.project.service.OrderService;
+import com.ecommerce.project.util.AuthUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/order/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod, @RequestBody OrderRequestDTO orderRequestDTO) {
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO orderDTO = orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(orderDTO, HttpStatus.OK);
+    }
+
+}
